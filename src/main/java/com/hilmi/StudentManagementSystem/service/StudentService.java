e5b6005c747dc3d5c24121b8933eef5630b3e0a0
Hilmi Baskoparan <h.baskoparan@gmail.com>
2024-01-13 14:17:25 +0300

    Student Service and Service Impl

---
+++ src/main/java/com/hilmi/StudentManagementSystem/service/StudentService.java
@@ -0,0 +1,19 @@
+package com.hilmi.StudentManagementSystem.service;
+
+import com.hilmi.StudentManagementSystem.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+
+    List<Student> getAllStudents();
+
+    Student addStudent(Student student);
+
+    Student updateStudent(Student student);
+
+    Student getStudentById(Long id);
+
+    void deleteStudentById(Long id);
+
+}
---
+++ src/main/java/com/hilmi/StudentManagementSystem/service/imp/StudentServiceImpl.java
@@ -0,0 +1,43 @@
+package com.hilmi.StudentManagementSystem.service.imp;
+
+import com.hilmi.StudentManagementSystem.entity.Student;
+import com.hilmi.StudentManagementSystem.repository.StudentRepository;
+import com.hilmi.StudentManagementSystem.service.StudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
