4b4b03c1ab1280bcd5d5ec1403815a699ec4ba05
Hilmi Baskoparan <h.baskoparan@gmail.com>
2024-01-20 13:42:23 +0300

    Edit Repository

--- src/main/java/com/hilmi/StudentManagementSystem/repository/StudentRepository.java
+++ src/main/java/com/hilmi/StudentManagementSystem/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.hilmi.StudentManagementSystem.repository;
 
 import com.hilmi.StudentManagementSystem.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
